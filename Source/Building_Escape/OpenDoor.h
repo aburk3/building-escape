27a4e88bbffb3bc4560b1b7b2eff047090a9433f
Austin Burke <aburk3@gmail.com>
2021-09-29 05:50:06 -0500

    Refactoring to set open door action on pawn actor

--- Source/Building_Escape/OpenDoor.h
+++ Source/Building_Escape/OpenDoor.h
@@ -24,6 +24,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenDoor(float DeltaTime);
 
 private:
 	float InitialYaw;
@@ -34,4 +35,7 @@ private:
 
 	UPROPERTY(EditAnywhere)
 	ATriggerVolume* PressurePlate;
+
+	UPROPERTY(EditAnywhere)
+	AActor* ActorThatOpens;
 };
